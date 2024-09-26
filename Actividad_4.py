7b9dec840158e8421a22ffeb438086057edcafe1
Lazoangel <a.23308051281190@cbtis128.edu.mx>
2024-09-25 20:31:05 -0600

    Add files via upload

---
+++ Actividad_4.py
@@ -0,0 +1,6 @@
+print ("Lazo Jimenez Angel Jesus 3W")
+num1 =float(input("ingresar primer numero: ")) #Pide ingresar el primero numero
+num2 =float(input("ingresar segundo numero: ")) #Pide ingresar el segundo numero
+num3 =float(input("ingresar tercer numero: ")) #Pide ingresar el tercer numero
+resultado = (num1 + num2 + num3) /3 #Es la formula para sacar el promedio
+print (f"el promedio de {num1} {num2} {num3} es {resultado}: ") #Muestra el resultado de la formula para sacar el promedio
